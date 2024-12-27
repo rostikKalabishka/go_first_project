15fdc3bd8ca5c03a93dec1401e92040f4e42be70
rostikKalabishka <rostik31052002@gmail.com>
2024-12-27 17:07:17 +0200

    change

--- variables/variables.go
+++ variables/variables.go
@@ -2,9 +2,22 @@ package main
 
 import "fmt"
 
-var c, python, java bool
-
 func main() {
-	var i int
-	fmt.Println(i, c, python, java)
+
+    var a = "initial"
+    fmt.Println(a)
+
+    var b, c int = 1, 2
+    fmt.Println(b, c)
+
+    var d = true
+    fmt.Println(d)
+
+    var e int
+    fmt.Println(e)
+
+
+	// таке скорочення тільвки в  ф-ї
+    f := "apple"
+    fmt.Println(f)
 }
